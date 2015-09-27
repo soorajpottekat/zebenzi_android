8ac2c655b92a523b905b09b60b90ff779f268d3f
Vaugan Nayagar <vaugann@gmail.com>
2015-09-27 19:28:01 +0200

    Navigation drawer working

--- app/src/main/java/com/zebenzi/zebenzi/MainActivity.java
+++ app/src/main/java/com/zebenzi/zebenzi/MainActivity.java
@@ -1,12 +1,16 @@
 package com.zebenzi.zebenzi;
 
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentManager;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +18,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
@@ -26,6 +35,14 @@ public class MainActivity extends ActionBarActivity implements FragmentListener{
     public static Context appContext;
     private FragmentManager fm = getSupportFragmentManager();
     private String mSearchString;
+    private String[] mPlanetTitles = {"Search", "Account", "History", "Login", "Register"};
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle = "drawer title";
+    private CharSequence mTitle = "drawer title";
 
     /**
      * Keep track of the search task to ensure we can cancel it if requested.
@@ -37,14 +54,15 @@ public class MainActivity extends ActionBarActivity implements FragmentListener{
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_options, menu);
 
-        // Associate searchable configuration with the SearchView
+        // Associate searchable configuration with the SearchView and Toolbar
+
         SearchManager searchManager =
                 (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        MenuItem searchItem = menu.findItem(R.id.search);
+        MenuItem searchItem = menu.findItem(R.id.toolbar_search);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
         searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false);
+        searchView.setIconifiedByDefault(true);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -82,30 +100,99 @@ public class MainActivity extends ActionBarActivity implements FragmentListener{
         getSupportActionBar().setIcon(R.drawable.ic_menu_zebenzi);
         appContext = getApplicationContext();
 
+
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        // set a custom shadow that overlays the main content when the drawer opens
+//        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.list_row_navigation_drawer, mPlanetTitles));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
         handleIntent(getIntent());
-
-
-
     }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+//        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+
         FragmentTransaction transaction = fm.beginTransaction();
+
 
         // Replace whatever is in the fragment_container view with this fragment,
         // and add the transaction to the back stack so the user can navigate back
         switch (item.getItemId()) {
+            case R.id.action_websearch:
+                // create intent to perform web search for this planet
+//                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+//                intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
+//                // catch event that there's no activity to handle intent
+//                if (intent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(intent);
+//                } else {
+                    Toast.makeText(this, "TESTING!", Toast.LENGTH_LONG).show();
+//                }
+                return true;
             case R.id.action_search:
                 SearchResultsFragment searchFragment = new SearchResultsFragment();
                 transaction.replace(R.id.fragment_container, searchFragment);
                 transaction.addToBackStack(null);
                 transaction.commit();
+                getSupportActionBar().setTitle(R.string.search_title);
                 return true;
             case R.id.action_history:
                 JobHistoryFragment historyFragment = new JobHistoryFragment();
                 transaction.replace(R.id.fragment_container, historyFragment);
                 transaction.addToBackStack(null);
                 transaction.commit();
+                getSupportActionBar().setTitle(R.string.history);
                 return true;
             case R.id.action_account:
                 return true;
@@ -114,12 +201,16 @@ public class MainActivity extends ActionBarActivity implements FragmentListener{
                 transaction.replace(R.id.fragment_container, registerFragment);
                 transaction.addToBackStack(null);
                 transaction.commit();
+                getSupportActionBar().setTitle(R.string.register);
+
                 return true;
             case R.id.action_login:
                 LoginFragment loginFragment = new LoginFragment();
                 transaction.replace(R.id.fragment_container, loginFragment);
                 transaction.addToBackStack(null);
                 transaction.commit();
+                getSupportActionBar().setTitle(R.string.login);
+
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -212,6 +303,69 @@ public class MainActivity extends ActionBarActivity implements FragmentListener{
                 System.out.println("Fragment not found. Id = " + fragmentId);
                 break;
         }
+    }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+
+        int id = R.id.action_search;
+        switch (position) {
+            case 0:
+                id = R.id.action_search;
+                break;
+            case 1:
+                id = R.id.action_account;
+                break;
+            case 2:
+                id = R.id.action_history;
+                break;
+            case 3:
+                id = R.id.action_login;
+                break;
+            case 4:
+                id = R.id.action_register;
+                break;
+        }
+
+        changeFragment(id);
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+//        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
 
