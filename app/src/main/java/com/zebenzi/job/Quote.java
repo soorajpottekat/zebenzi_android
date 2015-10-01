608b72e5b7db1f54f199060b9ee5925174fd05c1
Vaugan Nayagar <vaugann@gmail.com>
2015-10-01 23:15:14 +0200

    Quote might need to save the date and time

--- app/src/main/java/com/zebenzi/job/Quote.java
+++ app/src/main/java/com/zebenzi/job/Quote.java
@@ -2,6 +2,9 @@ package com.zebenzi.job;
 
 import com.zebenzi.Service.Services;
 
+import java.sql.Time;
+import java.util.Date;
+
 /**
  * Created by Vaugan.Nayagar on 2015/10/01.
  *
@@ -10,12 +13,19 @@ import com.zebenzi.Service.Services;
 public class Quote {
     Services svc;
     int units;
+    Date date;
+    Time time;
 
+    public Quote(Services svc, int units, Date date, Time time) {
+        this.svc = svc;
+        this.units = units;
+        this.date = date;
+        this.time = time;
+    }
     public Quote(Services svc, int units) {
         this.svc = svc;
         this.units = units;
     }
-
     public float getPrice() {
         return this.units * this.svc.getUnitPrice();
     }
