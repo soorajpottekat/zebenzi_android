8f6910ee4f6858f3c01dabc90af2936d8702a436
Vaugan Nayagar <vaugann@gmail.com>
2015-10-11 21:27:25 +0200

    Use dropdown instead of edit text for units

--- app/src/main/java/com/zebenzi/ui/NewJobFragment.java
+++ app/src/main/java/com/zebenzi/ui/NewJobFragment.java
@@ -8,10 +8,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
@@ -21,12 +21,10 @@ import com.zebenzi.users.Customer;
 import com.zebenzi.utils.DatePickerFragment;
 import com.zebenzi.utils.TimePickerFragment;
 
-import java.sql.Time;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import static com.zebenzi.ui.FragmentsLookup.SEARCH;
@@ -49,6 +47,10 @@ public class NewJobFragment extends Fragment {
     private Button jobTime;
     GregorianCalendar mDate;
     GregorianCalendar mTime;
+    private Spinner spinnerService;
+    private Spinner spinnerUnits;
+    ArrayList<String> unitsSpinnerArray = new ArrayList<String>();
+    ArrayAdapter<String> unitsSpinnerArrayAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -56,22 +58,10 @@ public class NewJobFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_new_job, container, false);
 
-        final Spinner spinnerService = (Spinner) rootView.findViewById(R.id.new_job_service_name);
-        ArrayList<String> spinnerArray= new ArrayList<String>() {
-            {
-                //TODO: Make this build dynamically from the enum
-                add(Services.GARDENER.getName());
-                add(Services.TILER.getName());
-                add(Services.PAINTER.getName());
-                add(Services.CLADDER.getName());
-                add(Services.PAVER.getName());
-                add(Services.PLASTERER.getName());
-                add(Services.PLUMBER.getName());
-            }};
 
         //Select date via datepicker fragment
-        jobDate = (Button)rootView.findViewById(R.id.new_job_date);
-        jobTime = (Button)rootView.findViewById(R.id.new_job_time);
+        jobDate = (Button) rootView.findViewById(R.id.new_job_date);
+        jobTime = (Button) rootView.findViewById(R.id.new_job_time);
 
         DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy");
         mDate = new GregorianCalendar();
@@ -95,19 +85,42 @@ public class NewJobFragment extends Fragment {
             }
         });
 
+        //Spinner for list of services
+        spinnerService = (Spinner) rootView.findViewById(R.id.new_job_service_name);
+        ArrayList<String> spinnerArray = new ArrayList<String>() {
+            {
+                for(Services svc: Services.values()){
+                    add(svc.getName());}
+            }
+        };
         ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(MainActivity.getAppContext(), R.layout.spinner_item, spinnerArray);
         spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         spinnerService.setAdapter(spinnerArrayAdapter);
+        spinnerService.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updateUnits();
+            }
 
-        final EditText units = (EditText) rootView.findViewById(R.id.new_job_units);
-        units.setText("1");
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //do nothing
+            }
+        });
 
-        Button buttonGetQuote = (Button)  rootView.findViewById(R.id.new_job_get_quote);
+
+
+        //Spinner for number of units
+        spinnerUnits = (Spinner) rootView.findViewById(R.id.new_job_units_spinner);
+        updateUnits();
+
+
+        Button buttonGetQuote = (Button) rootView.findViewById(R.id.new_job_get_quote);
         buttonGetQuote.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 String item = spinnerService.getSelectedItem().toString();
                 Services quoteSvc = Services.of(item);
-                int quoteUnits = Integer.parseInt(units.getText().toString());
+                int quoteUnits = Integer.parseInt(spinnerUnits.getSelectedItem().toString());
                 Quote q = new Quote(quoteSvc, quoteUnits, mDate, mTime);
                 System.out.println("Quote price = " + q.getPrice());
                 Customer.getInstance().setCurrentQuote(q);
@@ -117,6 +130,27 @@ public class NewJobFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    private void updateUnits() {
+        unitsSpinnerArray.clear();
+        Services svc = Services.of(spinnerService.getSelectedItem().toString());
+        int increment;
+        int units = 0;
+        increment = svc.getIncrement();
+
+        //TODO: Move the logic to server
+        for (int i = 1; i <= svc.getMaxUnits(); i++) {
+            units = (i * increment);
+            if (units < svc.getMaxUnits()) {
+                unitsSpinnerArray.add(Integer.toString(units));
+            } else {
+                break;
+            }
+        }
+        unitsSpinnerArrayAdapter = new ArrayAdapter<>(MainActivity.getAppContext(), R.layout.spinner_item, unitsSpinnerArray);
+        unitsSpinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spinnerUnits.setAdapter(unitsSpinnerArrayAdapter);
     }
 
 
@@ -193,6 +227,31 @@ public class NewJobFragment extends Fragment {
         mTime = new GregorianCalendar(mHour, mMonth, mDay, mHour, mMin);
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
         jobTime.setText(sdf.format(mTime.getTime()));
+    }
+
+    /**
+     * For the spinner, we can't just display a linear increment.
+     * So we have a sliding scale.
+     *     1-20 = x1
+     *     21-30 = x5
+     *     31-50 = x10
+     *     51-100 = x20
+     * @param spinnerCounter
+     * @return
+     */
+    public int getUnit(int spinnerCounter, int increment) {
+
+        if (spinnerCounter <= 20) {
+            return spinnerCounter * increment * spinnerCounter;
+        } else if (spinnerCounter <= 30) {
+            return spinnerCounter * increment * spinnerCounter * 5;
+        } else if (spinnerCounter <= 50) {
+            return spinnerCounter * increment * spinnerCounter * 10;
+        } else if (spinnerCounter <= 100) {
+            return spinnerCounter * increment * spinnerCounter * 20;
+        }
+
+        return 0;
     }
 }
 
